99a7fe8781b152ed1db53577c50729ccd90b1203
SamuelWr <samuel.wraak@gmail.com>
2018-12-02 15:41:57 +0100

    Day 2 - Parse input

---
+++ Day2/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Day2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] input = System.IO.File.ReadAllLines("input.txt");
+        }
+    }
+}
