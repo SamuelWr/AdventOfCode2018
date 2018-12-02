061247e560f5880ddaa1498f0f7b7af7e813953f
SamuelWr <samuel.wraak@gmail.com>
2018-12-02 15:54:16 +0100

    Day 2 - A

--- Day2/Program.cs
+++ Day2/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace Day2
 {
@@ -7,6 +9,25 @@ namespace Day2
         static void Main(string[] args)
         {
             string[] input = System.IO.File.ReadAllLines("input.txt");
+            A(input);
+        }
+
+        static void A(string[] input)
+        {
+            int exactlyTwo = input.Count(l => HasCharOccuringExactly(l, 2));
+            int exactlyThree = input.Count(l => HasCharOccuringExactly(l, 3));
+            Console.WriteLine("A: " + (exactlyTwo * exactlyThree));
+        }
+
+        private static bool HasCharOccuringExactly(string boxId, int times)
+        {
+            var count = new Dictionary<char, int>();
+            foreach (var character in boxId)
+            {
+                if (!count.TryAdd(character, 1))
+                    count[character]++;
+            }
+            return count.Values.Any(v => v == times);
         }
     }
 }
