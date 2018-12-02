9f78ff2420adc1ba73b803fcb93842eb2bb244ac
SamuelWr <samuel.wraak@gmail.com>
2018-12-02 15:39:36 +0100

    Day 1 - B

--- Day1/Program.cs
+++ Day1/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System.Linq;
 using System;
+using System.Collections.Generic;
 
 namespace Day1
 {
@@ -11,11 +12,43 @@ namespace Day1
                 .Where(l => !string.IsNullOrWhiteSpace(l))
                 .Select(l => int.Parse(l)).ToArray();
             A(input);
+            B(input);
         }
 
         static void A(int[] input)
         {
             Console.WriteLine("A: " + input.Sum());
+        }
+        static void B(int[] input)
+        {
+            int frequency = 0;
+            HashSet<int> seenFrequencies = new HashSet<int>();
+            seenFrequencies.Add(frequency);
+            foreach (var change in Loop(input))
+            {
+                frequency += change;
+                if (seenFrequencies.Contains(frequency))
+                {
+                    Console.WriteLine("B: first repeat: " + frequency);
+                    return;
+                }
+                else
+                {
+                    seenFrequencies.Add(frequency);
+                }
+            }
+            Console.WriteLine("B: No repeats");
+        }
+
+        static IEnumerable<T> Loop<T>(IEnumerable<T> Source)
+        {
+            while (true)
+            {
+                foreach (var item in Source)
+                {
+                    yield return item;
+                }
+            }
         }
     }
 }
