f9bdb4909f560609c1f6ab713b4a3ff1fd09e6ec
SamuelWr <samuel.wraak@gmail.com>
2018-12-08 15:41:30 +0100

    Day 7 - Test for A

--- Day7/Program.cs
+++ Day7/Program.cs
@@ -9,7 +9,7 @@ namespace Day7
         static void Main(string[] args)
         {
             var input = ParseInput(System.IO.File.ReadLines("input.txt"));
-            Console.WriteLine(input.Last());
+            Test();
         }
 
         private static (char step, char requires)[] ParseInput(IEnumerable<string> input)
@@ -21,6 +21,27 @@ namespace Day7
                 .Select(l => regex.Match(l))
                 .Select(match => (match.Groups[2].Value.Single(), match.Groups[1].Value.Single()))
                 .ToArray();
+        }
+
+        private static void Test()
+        {
+            var testInput = @"Step C must be finished before step A can begin.
+Step C must be finished before step F can begin.
+Step A must be finished before step B can begin.
+Step A must be finished before step D can begin.
+Step B must be finished before step E can begin.
+Step D must be finished before step E can begin.
+Step F must be finished before step E can begin.";
+            var input = ParseInput(testInput.Split(new[] { '\n' }).Select(l => l.Trim()));
+
+            string resA = A(input);
+            if (resA == "CABDFE") Console.WriteLine("Test A OK");
+            else Console.WriteLine("Test A FAIL, returned \"" + resA + "\" expected \"CABDFE\"");
+        }
+
+        private static string A((char step, char requires)[] input)
+        {
+            return "";
         }
     }
 }
