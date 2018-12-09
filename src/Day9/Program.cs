8dd6b559b5d1047d73d406504ba8f62266767e74
SamuelWr <samuel.wraak@gmail.com>
2018-12-09 10:12:07 +0100

    Day 9 - Structure and tests for A

---
+++ src/Day9/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Day9
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+
+        public static int A(int numPlayers, int maxMarble)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
