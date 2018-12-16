9de8c1b9146a5e1991dbc9607ab585cb8386a710
SamuelWr <samuel.wraak@gmail.com>
2018-12-16 18:56:41 +0100

    Day 14 - B

--- src/Day14/Program.cs
+++ src/Day14/Program.cs
@@ -8,59 +8,153 @@ namespace Day14
     {
         static void Main(string[] args)
         {
-            int input = 440231;
+            string input = "440231";
             Test();
 
             var watch = System.Diagnostics.Stopwatch.StartNew();
             Console.WriteLine("A: ");
-            Console.WriteLine("\tresult: " + A(input));
-            Console.WriteLine("\tin: " +watch.ElapsedMilliseconds+"ms");
+            Console.WriteLine("\tresult: " + A(int.Parse(input)));
+            Console.WriteLine("\tin: " + watch.ElapsedMilliseconds + "ms");
+
+            watch.Restart();
+            Console.WriteLine("B: ");
+            Console.WriteLine("\tresult: " + B(input));
+            Console.WriteLine("\tin: " + watch.ElapsedMilliseconds + "ms");
         }
 
         private static void Test()
         {
             bool ok = true;
-            int input;
+            int inputA;
             string resA;
-            string expected;
+            string expectedA;
             //If the Elves think their skill will improve after making 9 recipes, the scores of the ten recipes after the first nine on the scoreboard would be 5158916779 (highlighted in the last line of the diagram).
-            input = 9;
-            expected = "5158916779";
-            resA = A(input);
-            if (resA != expected)
+            inputA = 9;
+            expectedA = "5158916779";
+            resA = A(inputA);
+            if (resA != expectedA)
             {
                 ok = false;
-                Console.WriteLine($"Test A Failed for input {input}! result was {resA}, expected {expected}");
+                Console.WriteLine($"Test A Failed for input {inputA}! result was {resA}, expected {expectedA}");
             }
             //After 5 recipes, the scores of the next ten would be 0124515891.
-            input = 5;
-            expected = "0124515891";
-            resA = A(input);
-            if (resA != expected)
+            inputA = 5;
+            expectedA = "0124515891";
+            resA = A(inputA);
+            if (resA != expectedA)
             {
                 ok = false;
-                Console.WriteLine($"Test A Failed for input {input}! result was {resA}, expected {expected}");
+                Console.WriteLine($"Test A Failed for input {inputA}! result was {resA}, expected {expectedA}");
             }
             //After 18 recipes, the scores of the next ten would be 9251071085.
-            input = 18;
-            expected = "9251071085";
-            resA = A(input);
-            if (resA != expected)
+            inputA = 18;
+            expectedA = "9251071085";
+            resA = A(inputA);
+            if (resA != expectedA)
             {
                 ok = false;
-                Console.WriteLine($"Test A Failed for input {input}! result was {resA}, expected {expected}");
+                Console.WriteLine($"Test A Failed for input {inputA}! result was {resA}, expected {expectedA}");
             }
             //After 2018 recipes, the scores of the next ten would be 5941429882.
-            input = 2018;
-            expected = "5941429882";
-            resA = A(input);
-            if (resA != expected)
+            inputA = 2018;
+            expectedA = "5941429882";
+            resA = A(inputA);
+            if (resA != expectedA)
             {
                 ok = false;
-                Console.WriteLine($"Test A Failed for input {input}! result was {resA}, expected {expected}");
+                Console.WriteLine($"Test A Failed for input {inputA}! result was {resA}, expected {expectedA}");
             }
 
             if (ok) Console.WriteLine("Test A: OK");
+
+
+            //B
+            ok = true;
+            string inputB;
+            int resB;
+            int expectedB;
+
+            //51589 first appears after 9 recipes.
+            inputB = "51589";
+            expectedB = 9;
+            resB = B(inputB);
+            if (resB != expectedB)
+            {
+                ok = false;
+                Console.WriteLine($"Test B Failed for input {inputB}! result was {resB}, expected {expectedB}");
+            }
+
+            //01245 first appears after 5 recipes.
+            inputB = "01245";
+            expectedB = 5;
+            resB = B(inputB);
+            if (resB != expectedB)
+            {
+                ok = false;
+                Console.WriteLine($"Test B Failed for input {inputB}! result was {resB}, expected {expectedB}");
+            }
+
+            //92510 first appears after 18 recipes.
+            inputB = "92510";
+            expectedB = 18;
+            resB = B(inputB);
+            if (resB != expectedB)
+            {
+                ok = false;
+                Console.WriteLine($"Test B Failed for input {inputB}! result was {resB}, expected {expectedB}");
+            }
+
+            //59414 first appears after 2018 recipes.
+            inputB = "59414";
+            expectedB = 2018;
+            resB = B(inputB);
+            if (resB != expectedB)
+            {
+                ok = false;
+                Console.WriteLine($"Test B Failed for input {inputB}! result was {resB}, expected {expectedB}");
+            }
+
+            if (ok) Console.WriteLine("Test B: OK");
+        }
+
+        private static int B(string input)
+        {
+            int[] endSuffix = input.Select(c => int.Parse("" + c)).ToArray();
+
+            List<int> recipies = new List<int> { 3, 7 };
+            int elfOneRecipe = 0;
+            int elfTwoRecipe = 1;
+            while (true)
+            {
+                // new recipies
+                var sum = (recipies[elfOneRecipe] + recipies[elfTwoRecipe]).ToString(); ;
+
+                recipies.Add(int.Parse("" + sum[0]));
+                if (isEndCondition())
+                    return recipies.Count - input.Length;
+                if (sum.Length == 2)
+
+                {
+                    recipies.Add(int.Parse("" + sum[1]));
+                    if (isEndCondition())
+                        return recipies.Count - input.Length;
+                }
+                // elves change recipies
+                elfOneRecipe = (elfOneRecipe + 1 + recipies[elfOneRecipe]) % recipies.Count;
+                elfTwoRecipe = (elfTwoRecipe + 1 + recipies[elfTwoRecipe]) % recipies.Count;
+            }
+            bool isEndCondition()
+            {
+                if (recipies.Count < input.Length)
+                    return false;
+
+                for (int i = 0; i < input.Length; i++)
+                {
+                    if (endSuffix[input.Length - 1 - i] != recipies[recipies.Count - 1 - i])
+                        return false;
+                }
+                return true;                
+            }
         }
 
         static string A(int input)
@@ -74,7 +168,7 @@ namespace Day14
                 // new recipies
                 var sum = (recipies[elfOneRecipe] + recipies[elfTwoRecipe]).ToString(); ;
                 if (sum.Length == 1)
-                    recipies.Add(int.Parse(""+sum[0]));
+                    recipies.Add(int.Parse("" + sum[0]));
                 else
                 {
                     recipies.Add(int.Parse("" + sum[0]));
